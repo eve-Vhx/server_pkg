e90171db8d969d9d68f62fb1bb1cbc832e9fe94d
Henry Cappel <henry@evevehicles.com>
2023-02-16 16:49:32 -0600

    update

--- src/server_master.py
+++ src/server_master.py
@@ -6,8 +6,9 @@ class ClientConnect():
 
     def __init__(self,name):
         pi_connect_service = rospy.Service('pi_connect_master', masterConnect, self.handle_connect_cb)
+        print("Server connect service started!")
 
-    def handle_connect_cb(self, res):
+    def handle_connect_cb(self, req):
         print("recieved a pi!")
 
 
@@ -16,6 +17,7 @@ class ClientConnect():
 
 if __name__ == '__main__':
     rospy.init_node('server_master_node', anonymous=True)
+    ClientConnect(rospy.get_name())
 
 
 
