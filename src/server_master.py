2bd22146ab9419f001e833cb7957d917f90cd472
Henry Cappel <henry@evevehicles.com>
2023-02-16 17:40:38 -0600

    update

--- src/server_master.py
+++ src/server_master.py
@@ -2,7 +2,7 @@
 import rospy
 from msg_pkg.srv import masterConnect, masterConnectResponse
 
-class ClientConnect():
+class ClientConnect:
 
     def __init__(self,name):
         pi_connect_service = rospy.Service('pi_connect_master', masterConnect, self.handle_connect_cb)
@@ -18,6 +18,7 @@ class ClientConnect():
 if __name__ == '__main__':
     rospy.init_node('server_master_node', anonymous=True)
     ClientConnect(rospy.get_name())
+    rospy.spin()
 
 
 
