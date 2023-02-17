92e5a90247ef9d06157f0f9f54320d39c1c4ae52
Henry Cappel <henry@evevehicles.com>
2023-02-17 14:53:44 -0600

    update

--- src/action_client.py
+++ src/action_client.py
@@ -10,9 +10,18 @@ class ServerActionClient():
         print("Successfully started the action client")
         if(self.initialConnectPi()):
             print("Ready to send a goal to the action server")
+            self.sendInitialGoal()
         else:
             print("Could not connect to the action server for " + id)
     
     def initialConnectPi(self):
         return self.action_client_obj.wait_for_server(timeout = rospy.Duration(5.0))
+
+    def sendInitialGoal(self):
+        self.init_goal = server_px4_reqGoal(lat=0, lon=0, alt=0, yaw_rad=0, cruise_alt=0, mission_type=9, timestamp=rospy.Time.now())
+        self.action_client_obj.send_goal(self.init_goal)
+        self.action_client_obj.wait_for_result()
+        print("Result for the action server")
+        print(self.action_client_obj.get_result())
+
         
