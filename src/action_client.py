7e3982a0148afe384214d8efa06fab4d95ea6b25
Henry Cappel <henry@evevehicles.com>
2023-02-17 12:10:10 -0600

    started adding action client

--- src/action_client.py
+++ src/action_client.py
@@ -8,3 +8,8 @@ class ServerActionClient():
     def __init__(self):
         self.action_client_obj = actionlib.SimpleActionClient('/mavros/smr_px4_command/d1_cmd_action', server_px4_reqAction)
         print("Successfully started the action client")
+        self.initialConnectPi()
+    
+    def initialConnectPi(self):
+        self.action_client_obj.wait_for_server(timeout = rospy.Duration(5.0))
+        print("connected to the server")
