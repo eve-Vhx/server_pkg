71cb5520958fe3671bc7babb4473314d6530a3fd
Henry Cappel <henry@evevehicles.com>
2023-02-22 11:56:10 -0600

    update

--- src/arming_check.py
+++ src/arming_check.py
@@ -36,27 +36,27 @@ class ServerArmingCheck:
     def run_connection_checks(self):
         print("Inside run connection checks")
         if (self.connections_status["mavros"] == False):
-            # self.arming_feedback.feedback = 2
-            # self.arming_feedback.drone_id = self.drone_id
-            # self.arming_pub.publish(self.arming_feedback)
+            self.arming_feedback.feedback = 2
+            self.arming_feedback.drone_id = self.drone_id
+            self.arming_feedback_pub.publish(self.arming_feedback)
             print("Server arming request failed because mavros is offline") #publish to arming_feedback code: 2
             return False
         elif (self.connection_checks["px4"] == False):
-            # self.arming_feedback.feedback = 3
-            # self.arming_feedback.drone_id = self.drone_id
-            # self.arming_pub.publish(self.arming_feedback)
+            self.arming_feedback.feedback = 3
+            self.arming_feedback.drone_id = self.drone_id
+            self.arming_feedback_pub.publish(self.arming_feedback)
             print ("Server arming request failed because px4 is offline") #publish to arming_feedback code: 3
             return False
         elif (abs(rospy.Time.now().secs - self.connections_status["ros_timestamp"]) > 2):
-            # self.arming_feedback.feedback = 4
-            # self.arming_feedback.drone_id = self.drone_id
-            # self.arming_pub.publish(self.arming_feedback)
+            self.arming_feedback.feedback = 4
+            self.arming_feedback.drone_id = self.drone_id
+            self.arming_feedback_pub.publish(self.arming_feedback)
             print ("Server arming request failed because the time delay is too large for connection checks") #publish to arming_feedback code: 4
             return False
         else:
-            # self.arming_feedback.feedback = 5
-            # self.arming_feedback.drone_id = self.drone_id
-            # self.arming_pub.publish(self.arming_feedback)
+            self.arming_feedback.feedback = 5
+            self.arming_feedback.drone_id = self.drone_id
+            self.arming_feedback_pub.publish(self.arming_feedback)
             print ("Server arming request succeeded in connection checks") #publish to arming_feedback code: 5
             return True
 
@@ -68,13 +68,13 @@ class ServerArmingCheck:
                 if (self.request_arming == True):
                     self.arming_feedback.feedback = 0
                     self.arming_feedback.drone_id = self.drone_id
-                    self.arming_pub.publish(self.arming_feedback)
+                    self.arming_feedback_pub.publish(self.arming_feedback)
                     print("Server arming request succeeded!") #publish to arming_feedback code: 0 
                     self.arming_state.armed = True
                 else:
                     self.arming_feedback.feedback = 1
                     self.arming_feedback.drone_id = self.drone_id
-                    self.arming_pub.publish(self.arming_feedback)
+                    self.arming_feedback_pub.publish(self.arming_feedback)
                     print("Server disarming request successfully changed arming to disarmed") #publish to arming_feedback code: 1
                     self.arming_state.armed = False
             else:
