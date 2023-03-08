8b25ee06761f70c14572a6f9c0309aa538a360e1
Henry Cappel <henry@evevehicles.com>
2023-03-08 09:31:33 -0600

    added gps updates to nest connector

--- src/nest_connection.py
+++ src/nest_connection.py
@@ -3,10 +3,14 @@ import rospy
 from msg_pkg.srv import masterConnect, masterConnectResponse
 from msg_pkg.msg import NestBeaconActionFeedback, NestChargeActionFeedback
 from msg_pkg.msg import nestTelemMsg, fakeChargeMsg, fakeBeaconMsg
+from msg_pkg.srv import NestGPSMessage
+
+import threading
 
 
 class NestConnection:
     def __init__(self, id):
+        self.timer = threading.Timer(5,self.timeout)
         self.nest_connect_service = rospy.Service(id + 'nest_telem_connect', masterConnect, self.run_routine)
 
     def run_routine(self,req):
@@ -17,10 +21,22 @@ class NestConnection:
         self.charging = False
         self.beacon_on = False
         self.connected = False
+        self.gps = [0,0,0]
         self.nest_telem_pub = rospy.Publisher(self.id + '/ui_nest_telem', nestTelemMsg, queue_size=10)
         self.publish_nest_data()
 
+        self.timer.start()
+
         return masterConnectResponse(True)
+
+    def timeout(self):
+        rospy.wait_for_service(self.id + '/nest_gps')
+        try:
+            gps_client = rospy.ServiceProxy(self.id + '/nest_gps', NestGPSMessage)
+            gps_res = gps_client()
+            self.gps = [gps_res.latitude, gps_res.longitude, gps_res.altitude]
+        except rospy.ServiceException as e:
+            print("GPS Service call failed: %s"%e)
 
     def charging_cb(self,msg):
         self.charging = msg.feedback.charging
@@ -35,4 +51,5 @@ class NestConnection:
             nest_telem_msg.beacon = self.beacon_on
             nest_telem_msg.connected = self.connected
             self.nest_telem_pub.publish(nest_telem_msg)
+            print(self.gps)
             rospy.sleep(1)
